18cc97980924ba1ba4560a340219af7188d583ec
julie-autr <julie.autuoro@orange.fr>
2022-04-21 18:35:26 +0200

    k,,zv
    z,lef

--- morpion.js
+++ morpion.js
@@ -1,1 +1,10 @@
-console.log(document.getElementsByClassName('grille1'))
+window.addEventListener('resize',function(){
+    const width=document.querySelector('.grille').offsetWidth;
+    console.log(width);
+    document.querySelector('.megagrille').style.setProperty('grid-auto-rows',`${width}px`); 
+});
+window.addEventListener('load',function(){
+    const width=document.querySelector('.grille').offsetWidth;
+    console.log(width);
+    document.querySelector('.megagrille').style.setProperty('grid-auto-rows',`${width}px`);
+});
