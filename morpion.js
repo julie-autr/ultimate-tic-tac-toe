c0a16146be0c194b83d1657ece63cbded81be543
julie-autr <julie.autuoro@orange.fr>
2022-04-21 22:31:20 +0200

    yay design

--- morpion.js
+++ morpion.js
@@ -1,12 +1,28 @@
 const grilles=document.querySelectorAll('[class^="grille"]');
+const lignes=document.querySelectorAll('[class^="ligne"]');
 const cases=document.querySelectorAll('.case');
+const megagrille=document.querySelector('.megagrille')
 
+function estMultipleDeTrois(nombre){
+    if (Number.isInteger(nombre/3)){return true};
+    return false
+}
+var bigwidth=window.innerWidth/2;
+console.log(bigwidth);
 window.addEventListener('resize', () => {
+    bigwidth=window.innerWidth/2; console.log(bigwidth);
+    for (var i=0;i<3;i++){
+        if (estMultipleDeTrois(bigwidth+i)==true){console.log("ok",i,bigwidth+i),megagrille.style.setProperty('width',`${bigwidth+i}px`)}
+    };
     const width = cases[0].offsetWidth;
-    for (var i=0;i<cases.length;i++){grilles[i].style.setProperty('grid-auto-rows', `${width}px`)};  
-
+    for (var i=0;i<grilles.length;i++){grilles[i].style.setProperty('grid-auto-rows', `${width}px`)};  
 });
 window.addEventListener('load', () => {
+    for (var i=0;i<3;i++){
+        if (estMultipleDeTrois(bigwidth+i)==true){console.log("ok",bigwidth+i),megagrille.style.setProperty('width',`${bigwidth+i}px`)}
+    };
     const width = cases[0].offsetWidth;
-    for (var i=0;i<cases.length;i++){grilles[i].style.setProperty('grid-auto-rows', `${width}px`)};  
+    for (var i=0;i<grilles.length;i++){grilles[i].style.setProperty('grid-auto-rows', `${width}px`)
+};  
 });
+
