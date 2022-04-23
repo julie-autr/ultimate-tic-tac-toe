c73599f49f7d03cd9cab6313b52ce0f35a26eb75
julie-autr <julie.autuoro@orange.fr>
2022-04-23 23:19:48 +0200

    zelkfnl

--- morpion.js
+++ morpion.js
@@ -29,9 +29,39 @@ window.addEventListener('load', () => {
 const BoutonValider = document.getElementsByClassName("valider");
 let noms=[];
 
+var joueuractuel=0;
+
 document.getElementById("bouton").addEventListener('click',function(){
-    const nom1=document.getElementsByClassName("name1").value;
-    const nom2=document.getElementsByClassName("name2").value;
-    console.log(nom1,nom2);
+    const nom1=document.getElementsByClassName("name1")[0].value;
+    const nom2=document.getElementsByClassName("name2")[0].value;
+    document.getElementById("toDelete").style.display('none');
+    let nb=Math.random();
+    if (nb>0.5){document.getElementsByClassName("quicommence")[0].innerText=`C'est ${nom1} qui commence !`; joueuractuel=1}
+    else {document.getElementsByClassName("quicommence")[0].innerText=`C'est ${nom2} qui commence !`;joueuractuel=2};
 }
 );
+
+function griser(i){
+    for (var j=0;j<grilles.length;j++){grilles[j].style.backgroundColor="rgba(220,220,220,0.5)"};
+    grilles[i].style.backgroundColor='rgb(255,255,255)';
+};
+
+const Arraygrilles=Array.from(grilles);
+
+
+for (var i=0;i<grilles.length;i++){
+    grilles[i].addEventListener('click',function(event){
+        const target = event.target;
+        var parent=target.parentElement;
+        var indice=Arraygrilles.indexOf(parent);
+        griser(indice); 
+    })
+};
+
+for (var i=0;i<cases.length;i++){
+    cases[i].addEventListener('click',function(event){
+        const target = event.target;
+        var indice=Arraygrilles.indexOf(target);
+        griser(indice); 
+    })
+};
