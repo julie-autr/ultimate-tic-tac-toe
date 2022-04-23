fc96df7592556a56504141260ad02bbeae99ad2f
julie-autr <julie.autuoro@orange.fr>
2022-04-23 19:42:32 +0200

    zlfjb

--- morpion.js
+++ morpion.js
@@ -7,10 +7,9 @@ function estMultipleDeTrois(nombre){
     if (Number.isInteger(nombre/3)){return true};
     return false
 }
-var bigwidth=window.innerWidth/2;
-console.log(bigwidth);
+var bigwidth=2*window.innerWidth/5;
 window.addEventListener('resize', () => {
-    bigwidth=window.innerWidth/2; console.log(bigwidth);
+    bigwidth=2*window.innerWidth/5; console.log(bigwidth);
     for (var i=0;i<3;i++){
         if (estMultipleDeTrois(bigwidth+i)==true){console.log("ok",i,bigwidth+i),megagrille.style.setProperty('width',`${bigwidth+i}px`)}
     };
@@ -20,9 +19,19 @@ window.addEventListener('resize', () => {
 window.addEventListener('load', () => {
     for (var i=0;i<3;i++){
         if (estMultipleDeTrois(bigwidth+i)==true){console.log("ok",bigwidth+i),megagrille.style.setProperty('width',`${bigwidth+i}px`)}
+        else {megagrille.style.setProperty('width',`${Math.floor(bigwidth)}px`)}
     };
     const width = cases[0].offsetWidth;
     for (var i=0;i<grilles.length;i++){grilles[i].style.setProperty('grid-auto-rows', `${width}px`)
 };  
 });
 
+const BoutonValider = document.getElementsByClassName("valider");
+let noms=[];
+
+document.getElementById("bouton").addEventListener('click',function(){
+    const nom1=document.getElementsByClassName("name1").value;
+    const nom2=document.getElementsByClassName("name2").value;
+    console.log(nom1,nom2);
+}
+);
