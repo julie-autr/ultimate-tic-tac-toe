cf7e6cd8990a54522114d16fa78ee05572a63ac2
julie-autr <julie.autuoro@orange.fr>
2022-04-24 11:11:22 +0200

    qdklnqa

--- morpion.js
+++ morpion.js
@@ -80,19 +80,16 @@ for (var i=0;i<grilles.length;i++){
 
 var hover=0;
 function coup(g){ //g est l'indice de la grille dans laquelle on se situe, qui a déjà été grisée
-    
     var casesjouables=grilles[g].children // tableau des cases enfants de notre grille
     var Arraycasesjouables=Array.from(casesjouables);
     console.log(casesjouables, Arraycasesjouables)
     for (var i=0;i<casesjouables.length;i++){
+        casesjouables[i].onmouseover = function(){this.style.backgroundColor = "rgba(220,220,220,0.5)";};
+        casesjouables[i].onmouseout = function(){this.style.backgroundColor = "rgb(255,255,255)";};
         casesjouables[i].addEventListener('click',function(event){
             const target = event.target;
             var indice=Arraycasesjouables.indexOf(target);
-            console.log("case n°",indice);
-            griser(indice);
-            casesjouables[indice].style.backgroundColor='red';
-            hover+=1;
-            console.log(hover);
+            casesjouables[i].style.backgroundColor='red';
         });
-    };
+    }
 }
